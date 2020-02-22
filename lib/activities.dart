e2a587b107002cb826b36d87f1de1f813ba0ad87
keyclicker <kolchikcv@ukr.net>
2020-02-22 23:25:58 +0200

    Bad activities tab added

--- lib/activities.dart
+++ lib/activities.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'activity_tile.dart';
 
 enum ActivityType {
-  Running, Cycling, Walking,
+  Running,
+  Cycling,
+  Walking,
 }
 
 class Activities extends StatefulWidget {
@@ -17,8 +19,64 @@ class _ActivitiesState extends State<Activities> {
       children: <Widget>[
         Container(
           color: Colors.blue,
+          child: _drawInfo(),
         ),
         MyBottomSheet(),
+      ],
+    );
+  }
+
+  Widget _drawValue(double value, String sub) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+      child: Column(
+        children: <Widget>[
+          Text("${value}",
+              style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600
+              )),
+          Text(sub,
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600
+              ))
+        ],
+      ),
+    );
+  }
+
+
+  Widget _drawInfo() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Container(height: 50),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _drawValue(1.4, "km"),
+            _drawValue(21, "min"),
+            _drawValue(40, "km/h"),
+          ],
+        ),
+
+        Container(height: 20),
+
+        Text("Best resault for this track",
+            style: TextStyle(fontSize: 25, color: Colors.white)),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _drawValue(21, "min"),
+            _drawValue(40, "km/h"),
+          ],
+        ),
+
+
       ],
     );
   }
@@ -46,9 +104,20 @@ class _MyBottomSheetState extends State<MyBottomSheet> {
             child: ListView(
               controller: scrollController,
               children: <Widget>[
-                ActivityTile(type: ActivityType.Cycling,date: DateTime.now(), duration: Duration(minutes: 13), length: 3.5,),
-                ActivityTile(type: ActivityType.Running,date: DateTime.now(), duration: Duration(minutes: 21), length: 1.3,),
-                ActivityTile(type: ActivityType.Walking,date: DateTime.now(), duration: Duration(minutes: 42), length: 0.2,),
+                ActivityTile(type: ActivityType.Cycling,
+                  date: DateTime.now(),
+                  duration: Duration(minutes: 13),
+                  length: 3.5,),
+                ActivityTile(
+                  type: ActivityType.Running,
+                  date: DateTime.now(),
+                  duration: Duration(minutes: 21),
+                  length: 1.3,),
+                ActivityTile(
+                  type: ActivityType.Walking,
+                  date: DateTime.now(),
+                  duration: Duration(minutes: 42),
+                  length: 0.2,),
               ],
             ),
           );
