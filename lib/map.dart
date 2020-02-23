124fbae373332bf5b4b2661fae52e75b2cb88e6d
hryhorchuk117 <hryhorchuk117@gmail.com>
2020-02-23 03:59:09 +0200

    map fixed

--- lib/map.dart
+++ lib/map.dart
@@ -5,7 +5,7 @@ import 'package:location/location.dart';
 import 'dart:async';
 
 const double CAMERA_ZOOM = 16;
-const double CAMERA_TILT = 80;
+const double CAMERA_TILT = 0;
 const double CAMERA_BEARING = 30;
 const LatLng SOURCE_LOCATION = LatLng(42.747932,-71.167889);
 const LatLng DEST_LOCATION = LatLng(37.335685,-122.0605916);
